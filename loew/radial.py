6c16da49103f2139c83d6dab7579581cc07a3a48
Heitor Credidio <credidio@fisica.ufc.br>
2016-06-28 14:04:01 -0300

    Add dosctrings of radial routines

--- loew/radial.py
+++ loew/radial.py
@@ -2,6 +2,27 @@ import numpy as np
 
 
 def vslit_zip(z, dt, du):
+    '''
+    Vertical slit discretication of radial Loewner chains. This function is
+    the inverse of the solution of Loewner's Equation with driving function
+
+    U(t) = du, for 0 <= t <= dt
+
+    Parameters
+    ----------
+    z : complex or ndarray of complexes
+        Input array.
+
+    dt: float
+    du: float
+        Vertical slit parameters.
+
+    Returns
+    -------
+    w : complex or ndarray of complxes
+        Same shape as input array.
+
+    '''
     edu = np.exp(1j * du)
     D = np.exp(dt + 1j * du) * (z + 1.0) ** 2 / z
     C = np.sqrt(D ** 2 - 4.0 * edu * D)
@@ -11,24 +32,44 @@ def vslit_zip(z, dt, du):
     return w
 
 
-def trace(times, drive):
-    N = times.size
-    trace = np.ones(N, dtype=np.complex128)
-    for i in range(N - 1, 0, -1):
-        dt = times[i] - times[i - 1]
-        du = drive[i] - drive[i - 1]
-        trace[i:] = vslit_zip(trace[i:], dt, du)
-    return trace
+def trace(t, u):
+    '''
+    Compute the discretized Loewner trace of a driving function u(t) sampled at
+    discreet time instants. The trace is defined as a curve z(t) such that
+    z(t)= f(t, u(t)) where f(t, w) is the inverse of g(t, w), which is the
+    solution of the Loewner Equation in the unit disk
 
+    dg            exp{i u(t)} + g(t, w)
+    -- = g(t, w) -----------------------
+    dt            exp{i u(t)} - g(t, w)
 
-if __name__ == '__main__':
-    import matplotlib.pyplot as plt
-    from misc import brownian_motion
-    np.random.seed(100)
-    t = np.linspace(0.0, 10.0, 1000)
-    u = brownian_motion(t, kappa=2.0)
-    z = trace(t, u)
+    It makes use of the zipper algorithm with a vertical slit discretization
+    scheme.
 
-    fig, ax = plt.subplots()
-    ax.plot(z.real, z.imag)
-    plt.show()
+    It is considerably slower than the chordal and dipolar versions.
+
+    Reference
+    ---------
+    Kennedy, Tom. "Numerical computations for the Schramm-Loewner evolution."
+    Journal of Statistical Physics 137.5-6 (2009): 839-856.
+
+    Parameters
+    ----------
+    t: 1d ndarray of floats
+        Time instants where the driving is sampled.
+
+    u: 1d ndarray of floats
+        Sampled values of the driving function.
+
+    Returns
+    -------
+    z: 1d ndarrays of complex
+        Points of the trace at each time instant.
+    '''
+    n = t.size
+    z = np.ones(n, dtype=np.complex128)
+    for i in range(n - 1, 0, -1):
+        dt = t[i] - t[i - 1]
+        du = u[i] - u[i - 1]
+        z[i:] = vslit_zip(z[i:], dt, du)
+    return z
